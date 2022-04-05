7acd847d9e80c74e05419373783c46a9addd1d1d
vats <goktugcetin1@gmail.com>
2022-04-06 01:46:23 +0300

    F

---
+++ README.md
@@ -0,0 +1,23 @@
+# Govader Backend with Echo
+
+
+Govader-Backend is based on GoVader Package[https://github.com/jonreiter/govader](https://github.com/jonreiter/govader)
+
+
+## Usage:
+
+### Clone this Repo and run
+```sh
+git clone https://github.com/pimfiction/govader-backend-echo
+cd govader-backend-echo
+go run src/main.go
+```
+
+### Sample Get Request
+
+#### GET: http://localhost:8080?text=I%20am%20looking%20good
+
+### Sample Post Request 
+
+#### POST: http://localhost:8080/
+### RequestBody: ```{"text": "I am looking good"}```
---
+++ src/main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/jonreiter/govader"
+	echo "github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+type ResponseType struct {
+	Negative float64 `json:"Negative"`
+	Neutral  float64 `json:"Neutral"`
+	Positive float64 `json:"Positive"`
+	Compound float64 `json:"Compound"`
+}
+
+func TestHandler_HandleGetRequest(t *testing.T) {
+	tests := []struct {
+		name    string
+		h       Handler
+		wantErr bool
+	}{
+		{
+			"Successs",
+			Handler{analyzer: govader.NewSentimentIntensityAnalyzer()},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "Successs" {
+				httpRecorder := httptest.NewRecorder()
+				router := echo.New()
+				router.GET("/", tt.h.HandleGetRequest)
+				request, err := http.NewRequest("GET", "/?text=I am happy", nil)
+				assert.NoError(t, err)
+				router.ServeHTTP(httpRecorder, request)
+				assert.Equal(t, http.StatusOK, httpRecorder.Code)
+				var response ResponseType
+				body, err := ioutil.ReadAll(httpRecorder.Result().Body)
+				if err != nil {
+					t.Error(err)
+				}
+				_ = json.Unmarshal(body, &response)
+				assert.Equal(t, 1, 1)
+			}
+		})
+	}
+}
