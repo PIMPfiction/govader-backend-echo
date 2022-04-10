10d3b527cd611fc64298cc011cc2a9541a5b8606
vats <goktugcetin1@gmail.com>
2022-04-10 14:51:14 +0300

    heroku env

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	vaderMicro "github.com/PIMPfiction/govader_backend"
 	echo "github.com/labstack/echo/v4"
@@ -9,7 +10,7 @@ import (
 
 func main() {
 	e := echo.New()
-	err := vaderMicro.Serve(e, "80")
+	err := vaderMicro.Serve(e, os.Getenv("PORT"))
 	if err != nil {
 		panic(err)
 	}
